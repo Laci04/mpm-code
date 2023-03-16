b84e0d86ec3177c7c45341f6aeba7e9b6b01e3b1
Aqua <70912307+dot-aqua@users.noreply.github.com>
2023-03-16 16:39:32 +0100

    ULTRA Software Update (RT & Button States, sowie Position können nun ausgelesen werden).

--- mpm_brain_software/mpm_brain_software/mpm_brain_software.ino
+++ mpm_brain_software/mpm_brain_software/mpm_brain_software.ino
@@ -1,68 +1,66 @@
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
+#define ENCODER_A_PIN 18
+#define ENCODER_B_PIN 5
+#define BUTTON_PIN 34
+#define LCD_ADDRESS 0x27
+#define LCD_COLUMNS 20
+#define LCD_ROWS 4
 
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x3F, lcdColumns, lcdRows);  
+volatile int encoderPosition = 0;
+volatile bool encoderAPrevState = false;
+volatile bool encoderBPrevState = false;
+volatile bool buttonState = false;
 
-void setup(){
-  // initialize LCD
-  lcd.init();
-  // turn on LCD backlight                      
-  lcd.backlight();
+LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
+
+void ICACHE_RAM_ATTR encoderAISR() {
+  bool state = digitalRead(ENCODER_A_PIN);
+  if (state != encoderAPrevState) {
+    encoderAPrevState = state;
+    if (state && !encoderBPrevState) {
+      encoderPosition++;
+    } else if (!state && encoderBPrevState) {
+      encoderPosition--;
+    }
+  }
 }
 
-void loop(){
-  delay(1000);
-  lcd.setCursor(0, 0);
-  lcd.print("Verbindung wird");
-  lcd.setCursor(0, 1);
-  lcd.print("hergestellt.");
-  delay(150);
-    lcd.setCursor(0, 0);
-  lcd.print("Verbindung wird");
-  lcd.setCursor(0, 1);
-  lcd.print("hergestellt..");
-  delay(150);
-    lcd.setCursor(0, 0);
-  lcd.print("Verbindung wird");
-  lcd.setCursor(0, 1);
-  lcd.print("hergestellt...");
-  delay(150);
-  lcd.setCursor(0, 0);
-  lcd.print("Verbindung wird");
-  lcd.setCursor(0, 1);
-  lcd.print("hergestellt.");
-  delay(150);
-    lcd.setCursor(0, 0);
-  lcd.print("Verbindung wird");
-  lcd.setCursor(0, 1);
-  lcd.print("hergestellt..");
-  delay(150);
-    lcd.setCursor(0, 0);
-  lcd.print("Verbindung wird");
-  lcd.setCursor(0, 1);
-  lcd.print("hergestellt...");
-   delay(2000);
+void ICACHE_RAM_ATTR encoderBISR() {
+  bool state = digitalRead(ENCODER_B_PIN);
+  if (state != encoderBPrevState) {
+    encoderBPrevState = state;
+    if (state && !encoderAPrevState) {
+      encoderPosition--;
+    } else if (!state && encoderAPrevState) {
+      encoderPosition++;
+    }
+  }
+}
+
+void ICACHE_RAM_ATTR buttonISR() {
+  buttonState = !digitalRead(BUTTON_PIN);
+}
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  pinMode(ENCODER_A_PIN, INPUT_PULLUP);
+  pinMode(ENCODER_B_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A_PIN), encoderAISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_B_PIN), encoderBISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, CHANGE);
+}
+
+void loop() {
   lcd.clear();
-  // set cursor to first column, first row
-  lcd.setCursor(0, 0);
-  // print message
-  lcd.print("Smart Power Measurement!");
-  delay(1000);
+  lcd.setCursor(0,0);
+  lcd.print("Encoder Position:");
   lcd.setCursor(0,1);
-  lcd.print("Modus wählen:");
-  delay(2000);
-  lcd.clear(); 
-  delay(500);
-  lcd.setCursor(0, 0);
-  lcd.print("M1: Wirkleistungsverbrauch");
+  lcd.print(encoderPosition);
+  lcd.setCursor(10,1);
+  lcd.print(buttonState ? "Active" : "Inactive");
   delay(1000);
-  lcd.setCursor(0,1);
-  lcd.print("3.2 kWh");
-  delay(2000);
-  lcd.clear();
 }
